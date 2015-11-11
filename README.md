e9d59498880a9858ee484e9d5a5809341afd7bca
Adonis <adonis.settouf@gmail.com>
2015-11-11 17:02:44 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+symfony_test
+============
+
+A Symfony project created on November 10, 2015, 2:39 pm.
---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,46 @@
+<?php
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController extends Controller{
+
+  /**
+   * @Route("/test/number")
+   */
+   public function action(){
+     $number = rand(0,100);
+
+     return new Response('<html><body>Lucky number: '.$number.'</body></html>');
+   }
+
+   /**
+    * @Route("/test/numberjson")
+    */
+    public function jsonaction(){
+      $number = array(
+        "lucky number" => rand(0,100)
+      );
+
+
+      return new JsonResponse($number);
+    }
+
+    /**
+     * @Route("/test/number/{count}")
+     */
+    public function numberAction($count){
+      $number = array();
+      for ($i = 0; $i < $count; $i++){
+        $numbers[] = rand(0,100);
+      }
+    $responseText = "<html><body>Lucky numbers: ";
+    for ($i = 0; $i < $count; $i++){
+      $responseText = $responseText.$numbers[$i]." ";
+    }
+    $responseText = $responseText."</body></html>";
+    return new Response($responseText);
+  }
+}
